dbb949f717f7ad980bd315c406b5433514595125
STUART BUCHBINDER <stu@stubuchbinder.com>
2013-11-22 23:25:30 -0600

    Added Splash Scene and Main Scene Added basic SplashGUI script

---
+++ unity/Assets/Scripts/SplashGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashGUI : MonoBehaviour {
+	
+	
+	public string title = "Sisyphus";
+	public float playButtonWidth = 300.0f;
+	public float playButtonHeight = 100.0f;
+	
+	// Use this for gui display and interaction
+	void OnGUI()
+	{
+		// Play button loads 'main' scene
+		// for now just position in the middle of the screen
+		if(GUI.Button(new Rect(Screen.width / 2 - playButtonWidth / 2, Screen.height / 2 - playButtonHeight / 2, playButtonWidth, playButtonHeight), "Play")) {
+			Application.LoadLevel("main");
+		}
+	}
+}
