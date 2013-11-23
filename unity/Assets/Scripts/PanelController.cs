c80bb71b34d60baf22bb70211c17a3987a2171fb
STUART BUCHBINDER <stu@stubuchbinder.com>
2013-11-23 01:22:36 -0600

    Added logic to spawn nodes on play panel Reconstructed scene to orthographic camera mode for 2D

---
+++ unity/Assets/Scripts/PanelController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour {
+	
+	public GameObject nodePrefab;
+	public Transform gutter;
+	public GameObject highlight;
+	
+	private Vector3 _spawn;
+	private Vector3 _destination;
+	
+	void Start () 
+	{
+		gutter = GameObject.FindGameObjectWithTag("Gutter").transform;
+		
+		_spawn = new Vector3(gutter.localPosition.x + (gutter.localScale.x / 2), gutter.localPosition.y, gutter.localPosition.z);
+		_destination = gutter.localPosition;
+		
+		GameObject go = Instantiate(nodePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+		go.transform.parent = this.gameObject.transform;
+		go.transform.localPosition = _spawn;
+		go.transform.localScale = new Vector3(30, 29, 1);
+		//go.transform.localScale = Vector3.one;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
