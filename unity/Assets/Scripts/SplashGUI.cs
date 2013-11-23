67dc8731fd9b1892c8a57f606b8d6abe22fc883e
STUART BUCHBINDER <stu@stubuchbinder.com>
2013-11-23 00:17:10 -0600

    NGUI sprite library Updated Splash to Use NGUI sprite framework

--- unity/Assets/Scripts/SplashGUI.cs
+++ unity/Assets/Scripts/SplashGUI.cs
@@ -4,17 +4,9 @@ using System.Collections;
 public class SplashGUI : MonoBehaviour {
 	
 	
-	public string title = "Sisyphus";
-	public float playButtonWidth = 300.0f;
-	public float playButtonHeight = 100.0f;
-	
-	// Use this for gui display and interaction
-	void OnGUI()
+	public void pressedPlayButton(GameObject go)
 	{
-		// Play button loads 'main' scene
-		// for now just position in the middle of the screen
-		if(GUI.Button(new Rect(Screen.width / 2 - playButtonWidth / 2, Screen.height / 2 - playButtonHeight / 2, playButtonWidth, playButtonHeight), "Play")) {
-			Application.LoadLevel("main");
-		}
+		Debug.Log("pressedPlayButton");	
+		Application.LoadLevel("main");
 	}
 }
