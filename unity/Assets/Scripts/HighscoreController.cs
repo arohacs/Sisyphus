4150b6e3b3736b714a0cad43e051d275d328f557
STUART BUCHBINDER <stu@stubuchbinder.com>
2013-11-23 19:35:44 -0600

    Updated HighScoreController with highscore methods

--- unity/Assets/Scripts/HighscoreController.cs
+++ unity/Assets/Scripts/HighscoreController.cs
@@ -1,8 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HighscoreController : MonoBehaviour {
-
+public class HighscoreController : MonoBehaviour { 
+	
+	// these are the 10 UILabels on the scene
+	public UILabel highscoreName0;
+	public UILabel highscoreScore0;
+	
+	public UILabel highscoreName1;
+	public UILabel highscoreScore1;
+	
+	public UILabel highscoreName2;
+	public UILabel highscoreScore2;
+	
+	public UILabel highscoreName3;
+	public UILabel highscoreScore3;
+	
+	public UILabel highscoreName4;
+	public UILabel highscoreScore4;
+	
+	private string[] _highscores;
+	void Start()
+	{
+		_highscores = HighScores();
+		DisplayHighScores();
+	}
+	
+	// Highscores is stored as a string with '|' delimiter
+	// ex:  "Stu B-133ft | Adam R-52ft | Monty-50ft | Kate-25ft"
+	private string[] HighScores()
+	{
+		if (PlayerPrefs.HasKey("highscores")) {
+			string scores = PlayerPrefs.GetString("highscores");
+			return scores.Split("|"[0]);
+		}
+           // return PlayerPrefs.GetString("highscores").Split("-");
+        return new string[0];
+	}
+	
+	// Returns the name and score as an array of 2 elements
+	// ex: 
+	//     _highscores = GetHighScores();  
+	//		 _highscores[0] = "Stu B-133ft"
+	//   calling PlayerNameAndScore(_highscores[0]) 
+	//     return value is an array: [0] = Stu B
+	//                               [1] = 133ft
+	private string[] GetPlayerNameAndScore(string player) {
+		string[] retVal = player.Split("-"[0]);
+		return retVal;
+	}
+	
+	private void DisplayHighScores()
+	{
+		string[] score0 = GetPlayerNameAndScore(_highscores[0]);
+		highscoreName0.text = score0[0];
+		highscoreScore0.text = score0[1];
+		
+		// ...
+		// ...
+		// ...
+	}
 	public void pressedBackButton(GameObject go)
 	{
 		Application.LoadLevel("splash");	
